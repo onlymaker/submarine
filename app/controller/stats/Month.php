f1d711ed0903743d45656c0eebe5180c56e6cd11
jibo <jibo@outlook.com>
2016-03-19 11:43:31 +0800

    BUG#8

--- app/controller/stats/Month.php
+++ app/controller/stats/Month.php
@@ -12,7 +12,7 @@ class Month extends Base {
     function get() {
         $y = isset($_GET['y']) ? (int)$_GET['y'] : date('Y');
         $i = isset($_GET['i']) ? (int)$_GET['i'] : 1;
-        $t = isset($_GET['t']) ? (int)$_GET['t'] : 'shoe';
+        $t = isset($_GET['t']) ? $_GET['t'] : 'shoe';
         $helper = new Helper();
         $helper->statsWeekOrMonth($y, $i, $t, 'month');
     }
