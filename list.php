f1207b391db17ddf2aba4899ab7014cb6c4ba701
jibo <jibo@outlook.com>
2017-06-09 17:47:33 +0800

    Update list.php

--- list.php
+++ list.php
@@ -6,31 +6,46 @@ if (PHP_SAPI != 'cli') {
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$file = '/tmp/thumb';
+
 $f3 = Base::instance();
 
 $f3->config(__DIR__ . '/config/env.cfg');
-
-$file = '/tmp/thumb';
-$loop = 0;
-
-$prefix = '';
-$marker = '';
-$limit = 1000;
 
 $bucket = $f3->get('QINIU_BUCKET');
 
 $manager = new \Qiniu\Storage\BucketManager(new \Qiniu\Auth($f3->get('QINIU_ACCESS_KEY'), $f3->get('QINIU_SECRET_KEY')));
 
-echo 'Start', PHP_EOL;
+if ($argc == 1) {// list qiniu thumb
+    $loop = 0;
+    $prefix = '';
+    $marker = '';
+    $limit = 1000;
 
-while (!$loop || !empty($marker)) {
-    list($results, $marker, $error) = $manager->listFiles($bucket, $prefix, $marker, $limit);
-    empty($error) ? append($file, $results) : error($error);
-    $loop ++;
-    echo $loop, ':', $marker, PHP_EOL;
+    if (is_file($file)) {
+        unlink($file);
+    }
+
+    echo 'Start', PHP_EOL;
+
+    while (!$loop || !empty($marker)) {
+        list($results, $marker, $error) = $manager->listFiles($bucket, $prefix, $marker, $limit);
+        empty($error) ? append($file, $results) : error($error);
+        $loop ++;
+        echo $loop, ':', $marker, PHP_EOL;
+    }
+
+    echo 'Success', PHP_EOL;
+} else {// delete thumb file
+    if (is_file($file)) {
+        $content = file_get_contents($file);
+        $lines = explode(PHP_EOL, $content);
+        foreach ($lines as $line) {
+            echo 'delete:', $line, PHP_EOL;
+            $manager->delete($bucket, $line);
+        }
+    }
 }
-
-echo 'Success', PHP_EOL;
 
 function append($file, $results)
 {
