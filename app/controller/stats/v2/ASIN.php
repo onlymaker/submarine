546ba8ebfc76474ffb89a63e1efc3cb4fb5edb3d
jibo <jibo@outlook.com>
2018-07-07 23:32:01 +0800

    Add stock in china for asin stats

--- app/controller/stats/v2/ASIN.php
+++ app/controller/stats/v2/ASIN.php
@@ -76,11 +76,14 @@ class ASIN extends Base
             list($fbaResult) = $db->exec($sql);
             $sql = sprintf("SELECT count(*) as count FROM order_item o, prototype p WHERE p.model = '%s' AND p.ID = o.prototype_id AND o.channel = '%s' AND o.create_time > '%s' AND o.create_time < '%s'", $sku, $fbaChannel, $chainStart, $chainEnd);
             list($fbaChainResult) = $db->exec($sql);
+            $sql = sprintf("SELECT count(*) as count FROM stock s, prototype p WHERE s.location='中国' AND s.prototype_id=p.ID AND p.model='%s'", $sku);
+            list($stockInChina) = $db->exec($sql);
             $skuStats[$sku] = [
                 'count' => $result['count'],
                 'ratio' => $result['count'] && $chainResult['count'] ? sprintf('%.2f%%', ($result['count'] - $chainResult['count']) / $chainResult['count'] * 100) : '-',
                 'fbaCount' => $fbaResult['count'],
-                'fbaRatio' => $fbaResult['count'] && $fbaChainResult['count'] ? sprintf('%.2f%%', ($fbaResult['count'] - $fbaChainResult['count']) / $fbaChainResult['count'] * 100) : '-'
+                'fbaRatio' => $fbaResult['count'] && $fbaChainResult['count'] ? sprintf('%.2f%%', ($fbaResult['count'] - $fbaChainResult['count']) / $fbaChainResult['count'] * 100) : '-',
+                'china' => $stockInChina['count']
             ];
         }
 
--- app/tpl/stats/v2/asin_result.tpl
+++ app/tpl/stats/v2/asin_result.tpl
@@ -10,12 +10,13 @@
         <h1>父ASIN销售统计 <small>【{{strtoupper($data['head']['asin'])}}】【{{$data['head']['start']}}】【{{$data['head']['end']}}】</small></h1>
     </div>
     <table class="table table-striped">
-        <th>SKU</th><th colspan="2">{{$data['head']['channel']}}</th><th colspan="2">{{$data['head']['fbaChannel']}}</th>
+        <th>SKU</th><th colspan="2">{{$data['head']['channel']}}</th><th colspan="2">{{$data['head']['fbaChannel']}}</th><th>中国</th>
         {{foreach $data['sku'] as $name => $stats}}
             <tr>
                 <td>{{$name}}</td>
                 <td>{{$stats['count']}}</td><td>{{$stats['ratio']}}</td>
                 <td>{{$stats['fbaCount']}}</td><td>{{$stats['fbaRatio']}}</td>
+                <td>{{$stats['china']}}</td>
             </tr>
         {{/foreach}}
     </table>
